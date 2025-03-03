21b18788ecd491735176985ad5e6c807232be845
Robert <robert@michailov.org>
2025-03-02 18:33:58 -0800

    Hawk

--- Lesson 1: Flask and CRUD Basics/Homework/Skibidi.py
+++ Lesson 1: Flask and CRUD Basics/Homework/Skibidi.py
@@ -13,7 +13,7 @@ mongo = PyMongo(app)
 # NOTE: I see that you're storing the length. That is completely unnecessary in this case since the sets are so small. Just call len(list) to get.
 # NOTE: Why are you returning the whole object when you can just return the id of the set? Debugging right?
 @app.route('/user', methods=['POST'])
-def create_user():
+def create_set():
     data = request.json
 
     userSet = data.get('set')
@@ -27,8 +27,7 @@ def create_user():
         return jsonify({"error" : str(e)}), 500
 
     nums = {
-        'set' : userSet,
-        'length' : len(userSet)
+        'set' : userSet
     }
 
     try:
@@ -38,44 +37,92 @@ def create_user():
     except Exception as e:
         return jsonify({"error" : str(e)}), 500
 
-
-# # Route for update password <- Huh?
 # NOTE: Make two separate routes for adding and removing. Removing pops, up to you if you want to pop from the front or the back idrc. Adding appends
 # NOTE: $set is for changing things. You are appending to a list  when adding so using $push.
 @app.route('/user', methods=['PUT'])
-def update_password():
+def add_num():
     data = request.json
 
     setID = data.get('id')
     add = data.get('add')
-    remove = data.get('remove')
 
-    if not setID or (not add and not remove):
+    if not setID or not add:
         return jsonify({'error' : 'Missing required input'}), 400
     
     try:
         result = mongo.db.users.update_one(
-            {'username' : username},   # Object we are looking for
-            {"$set" : {'password' : newPassword}}   # Set means updating the object located and we are only changing password
+            {'_id' : ObjectId(setID)},   # Object we are looking for
+            {"$push" : {'set' : int(add)}}   # Set means updating the object located and we are only changing password
         )
 
         if result.matched_count == 0:
-            return jsonify({'error': "No user found"}), 404
+            return jsonify({'error': "No set found"}), 404
         elif result.modified_count == 0:
             return jsonify({"error" : "No change was made"}), 500
         else:
-            return jsonify({"msg" : f"Password for {username} has been updated."}), 200
+            return jsonify({"msg" : f"Set has been updated."}), 200
     except Exception as e:
         return jsonify({"error": str(e)})
 
+@app.route('/user', methods=['PUT'])
+def remove_num():
+    data = request.json
+
+    setID = data.get('id')
+    remove = data.get('remove')
+
+    if not setID or not remove:
+        return jsonify({'error' : 'Missing required input'}), 400
+    
+    try:
+        result = mongo.db.users.update_one(
+            {'_id' : ObjectId(setID)},   # Object we are looking for
+            {"$pull" : {'set' : int(remove)}}   # Set means updating the object located and we are only changing password
+        )
+
+        if result.matched_count == 0:
+            return jsonify({'error': "No set found"}), 404
+        elif result.modified_count == 0:
+            return jsonify({"error" : "No change was made"}), 500
+        else:
+            return jsonify({"msg" : f"Set has been updated."}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
+# @app.route('/user', methods=['PUT'])
+# def add_remove_num():
+#     data = request.json
+
+#     setID = data.get('id')
+#     add = data.get('add')
+#     remove = data.get('remove')
+
+#     if not setID or (not add and not remove) :
+#         return jsonify({'error' : 'Missing required input'}), 400
+    
+#     try:
+#         result = mongo.db.users.update_one(
+#             {'_id' : ObjectId(setID)},   # Object we are looking for
+#             {"$push" : {'set' : int(add)}}   # Set means updating the object located and we are only changing password
+#         )
+
+#         if result.matched_count == 0:
+#             return jsonify({'error': "No set found"}), 404
+#         elif result.modified_count == 0:
+#             return jsonify({"error" : "No change was made"}), 500
+#         else:
+#             return jsonify({"msg" : f"Set has been updated."}), 200
+#     except Exception as e:
+#         return jsonify({"error": str(e)})
 
 # NOTE: Please fix the function names and comments because it so hard to read.
 # NOTE: setData is a terrible name because it looks like that's what you're trying to set the data to.
 # NOTE: Your keys need to be consistent. You can't have message be dynamic. It should be something like "equation" : and then you can have a dynamic value
 # The whole point of key: value in maps is so that you can look in a consistent place for dynamic data.
-# Route for getting a user
+# Route for getting the result of operations on the set
 @app.route('/user', methods=['GET'])
-def get_user():
+def get_set():
     data = request.json
     
     operators = data.get('operators')
@@ -85,20 +132,20 @@ def get_user():
         return jsonify({"error": f"Arguments not provided\n{setID} {operators}"}), 400
     
     try:
-        setData = mongo.db.users.find_one({'_id' : ObjectId(setID)})
+        dataOfSet = mongo.db.users.find_one({'_id' : ObjectId(setID)})
 
-        if not setData:
+        if not dataOfSet:
             return jsonify({"error" : "No set found"}), 404
 
         if not operators:
-            return jsonify({"Set" : setData["set"]})
+            return jsonify({"Set" : dataOfSet["set"]})
 
         ops = operators.split(", ")
 
-        if len(ops) + 1 != len(setData["set"]):
+        if len(ops) + 1 != len(dataOfSet["set"]):
             return jsonify({"error" : "Wrong number of operations"}), 400
 
-        nums = setData["set"]
+        nums = dataOfSet["set"]
 
         total = nums[0]
         msg = f"{nums[0]}"
@@ -119,26 +166,14 @@ def get_user():
             else:
                 return jsonify({"error" : "Invalid input"}), 400
         
-        return jsonify({f"{msg}" : total})
+        return jsonify({"Equation" : f"{msg} = {total}"})
 
-        # if setData:
-        #     return jsonify(setData), 200
-        # else:
-        #     return jsonify({"error" : "No set found"}), 404
-        # # return jsonify({"msg": f"Found the user: {setData["set"]}"}), 200
-        # return jsonify(setData), 200
-
-    #     if setData:
-    #         user['_id'] = str(user['_id'])
-    #         return jsonify(user), 200
-    #     else:
-    #         return jsonify({"error" : "No set found"}), 404
     except Exception as e:
         return jsonify({"error" : str(e)}), 500
 
-# Delete a user
+# Delete a set
 @app.route('/user', methods=['DELETE'])
-def delete_user():
+def delete_set():
     data = request.json
     
     setID = data.get('id')
